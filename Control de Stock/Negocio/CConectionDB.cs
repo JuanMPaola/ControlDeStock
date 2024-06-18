8173584a461d820fa69f9f8b56af47d83fc1ce3f
JuanMPaola <juanmpaola25@gmail.com>
2024-06-18 18:42:01 -0300

    Conexion a la DB

---
+++ Control de Stock/Negocio/CConectionDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Negocio
+{
+    public class CConectionDB
+    {
+        SqlConnection conex = new SqlConnection();
+
+        static string servidor = "C2_15";
+        static string bd = "Otra";
+        
+        string cadenaConexion = "Data Source=" + servidor +";" + "Initial Catalog=" + bd + ";" + "Integrated Security =true";
+
+        public bool establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString = cadenaConexion;
+                conex.Open();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("No se logró conectar a la Base de Datos: " + e.ToString());
+                return false;
+            }
+        }
+
+        public DataSet traerTabla()
+        {
+            var consulta = "SELECT * FROM detalle"; // Asegúrate de que esta consulta sea correcta
+            var adaptador = new SqlDataAdapter(consulta, conex);
+
+            var commandBuilder = new SqlCommandBuilder(adaptador);
+            var ds = new DataSet();
+            adaptador.Fill(ds);
+
+            return ds;
+        }
+
+
+    }
+}
---
+++ Control de Stock/Vista/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio;
+
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+        CConectionDB conn = new CConectionDB();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isConnected = conn.establecerConexion();
+
+            if (isConnected)
+            {
+                MessageBox.Show("Conexión exitosa a la base de datos.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var ds = conn.traerTabla();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
